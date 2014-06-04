c42d98706aef34ac29a9c6de27df7ec68733a43d
Paul Colby <git@colby.id.au>
2014-06-04 18:35:41 +1000

    Some templating experiments.

--- include/prof/profile.hpp
+++ include/prof/profile.hpp
@@ -8,11 +8,36 @@
 
 /// @todo Lots
 
+template<class Decorant>
+class profiler_a {
+
+public:
+    static profiler_a * get_instance() { return instance; }
+
+private:
+    static profiler_a * instance;
+};
+
+template<class Decorant>
+class profiler_b : Decorant {
+
+public:
+    static profiler_b * get_instance() { return instance; }
+
+private:
+    static profiler_b * instance;
+};
+
+typedef profiler_a<profiler_b<int> > profiler;
+
+template<> profiler * profiler::instance(new profiler);
+
 #define PROFILE_MACRO_UUID 4029617a-ebbc-11e3-a02d-080027989a56
 
 #define PROFILE_BEGIN_SCOPE(...) \
     int todo_raii_object##PROFILE_MACRO_UUID=0; \
     todo_raii_object##PROFILE_MACRO_UUID++;   \
+    profiler::get_instance(); \
     /// @todo Begin scope.
 
 #endif // __PROFILE_PROFILE_HPP__

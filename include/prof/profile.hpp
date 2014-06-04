98df71de2a8cc8a167e7825626afe0f198d68172
Paul Colby <git@colby.id.au>
2014-06-04 17:34:58 +1000

    Empty trivial example program.

---
+++ include/prof/profile.hpp
@@ -0,0 +1,11 @@
+//               Copyright Paul Colby 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//       (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef __PROFILE_PROFILE_HPP__
+#define __PROFILE_PROFILE_HPP__
+
+/// @todo
+
+#endif

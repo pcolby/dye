3b1e2825eb0a43efec3771e7763c140e384e46f8
Paul Colby <git@colby.id.au>
2014-06-06 15:57:35 +1000

    Refactored the macros slightly.

--- include/dye/macros.hpp
+++ include/dye/macros.hpp
@@ -22,18 +22,17 @@
  */
 #define DYE_MACRO_UUID 4029617a-ebbc-11e3-a02d-080027989a56
 
+#define DYE_BEGIN_SCOPE(...) \
+    int todo_raii_object##DYE_MACRO_UUID=0; \
+    dye::dye_type::get_instance()->begin(__FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__); \
+    todo_raii_object##DYE_MACRO_UUID++;
+
 #define DYE_BEGIN_SECTION(...) \
     dye::dye_type::get_instance()->begin(__FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__); \
-    // auto-ender++?
+    todo_raii_object##DYE_MACRO_UUID++;
 
 #define DYE_END_SECTION() \
     dye::dye_type::get_instance()->end(); \
-    // auto-ender--?
-
-#define DYE_BEGIN_SCOPE(...) \
-    int todo_raii_object##DYE_MACRO_UUID=0; \
-    todo_raii_object##DYE_MACRO_UUID++;   \
-    dye::dye_type::get_instance()->begin(__FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__); \
-    // auto-ender++?
+    todo_raii_object##DYE_MACRO_UUID--;
 
 #endif
